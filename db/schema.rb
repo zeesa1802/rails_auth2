a468000498234c3aa8eb7db1d61dd651a48c8ad5
zeesa <zeesa18.2@gmail.com>
2022-04-12 13:21:24 +0500

    fix some styling issues

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_081149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
-  end
-
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -54,13 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_081149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_features_on_project_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
